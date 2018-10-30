e527656d3035c43eb6f225f86039ed95f952503d
Robin Vaysse <robinvaysse@hotmail.fr>
2018-10-30 16:04:35 +0100

    Add function to extract energy

--- src/features_extraction/extract_sound_windows.py
+++ src/features_extraction/extract_sound_windows.py
@@ -6,7 +6,6 @@ from tqdm import tqdm
 def create_time_csv(path, path_csv_out):
 
     files = list(sorted([file for file in os.listdir(path) if file.split('.')[-1] == 'xml']))
-    # print(files)
     time_csv = pd.DataFrame()
 
     for filename in tqdm(files):
@@ -34,30 +33,3 @@ def create_time_csv(path, path_csv_out):
 if __name__ == '__main__':
     path = './data/text/'
     create_time_csv(path, 'features/text/time_csv.csv')
-
-
-
-
-
-
-
-
-
-
-
-# list_id = set([id_.attrib['id'][:-1] for id_ in root.iter('time')])
-# #print(list_id)
-# columns = ['start', 'end', 'text']
-# time_csv = pd.DataFrame(index=list_id, columns=columns)
-
-# for time, text in zip(root.iter('time'), root.iter('w')):
-#     id_ = time.attrib['id'][:-1]
-
-#     print(text.text)
-#     print()
-#     if time.attrib['id'][-1] == 'S':
-#         time_csv.loc[id_, ['start', 'text']] = [time.attrib['value'], text.text]
-#     else:
-#         time_csv.loc[id_, 'end'] = time.attrib['value']
-
-# print(time_csv.head(6))
---
+++ src/features_extraction/nrj.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io.wavfile import read
+from scipy import signal
+
+def log_energie(Y, sr, win=512, step=256, plot=False):
+    """
+    Calcul de la log énergie d'un signal sur des fenêtres glissantes
+
+    Arguments:
+        Y {[numpy array]} -- signal dont on veut l'énergie
+        win {int} -- Taille des fenêtres glissantes en nombre de points (defaut: {512})
+        step {int} -- Taille du pas des fenêtres glissantes en nombre de points (defaut: {256})
+        plot {bool} -- Affiche ou non le résultat obtenu (defaut: {False})
+
+    Returns:
+        [list] -- Log énergie du signal
+    """
+
+    Y = Y.astype(np.float64)
+    N = len(Y)
+    nrj = []
+    for i in range(0, N, step):
+        if i+win < N:
+            nrj.append(np.log10(np.sum(np.power(Y[i:i+win], 2))))
+
+    if plot:
+        x = np.cumsum([step/sr] * len(nrj))
+        plt.figure(1, figsize=(12, 8))
+        plt.plot(x, nrj)
+        plt.title("Log(Enegie)")
+        plt.show()
+    return nrj
+
+if __name__ == '__main__':
+    f, Y = read('data/audio/SEQ_001_AUDIO.wav')
+    nrj = log_energie(Y, f, 4096, 2048, plot=True)
+
+    b, a = signal.butter(3, 0.1)
+    nrj = signal.filtfilt(b, a, nrj)
+
+    plt.plot(nrj)
+    plt.show()
