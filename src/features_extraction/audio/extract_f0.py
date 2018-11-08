1224bc220c1443f5c9cc471a73d4c8689bcb3320
Robin Vaysse <robinvaysse@hotmail.fr>
2018-11-08 09:34:59 +0100

    Add file to parse LIUM segmentation files

--- src/features_extraction/audio/extract_f0.py
+++ src/features_extraction/audio/extract_f0.py
@@ -3,14 +3,35 @@ import os
 from scipy.io.wavfile import read
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.io.wavfile import read
+from nrj import log_energie
 
 path_data = 'data/audio/'
-path_data = ''
+path_f0 = 'features/audio/f0/'
 
-files = [file for file in os.listdir(path) if file.split('.')[-1] == 'wav']
+files = sorted([file for file in os.listdir(path_data) if file.split('.')[-1] == 'wav'])
+f0_files = sorted([file for file in os.listdir(path_f0) if file.split('.')[-1] == 'f0'])
 
-f0_files = [file for file in os.listdir(path) if file.split('.')[-1] == 'f0']
+f0 = [x for x in pd.read_csv(path_f0 + f0_files[0], sep='\n').values]
 
+f0 = np.array(f0)[:, 0]
+print(f0.shape)
+b, a = signal.butter(3, 0.05)
+f0_filt = signal.filtfilt(b, a, f0)
+
+f0_filt = [1 if x > 10 else 0 for x in f0_filt]
+
+time = np.cumsum([0.01]*len(f0))
+
+f, sig = read(path_data + files[0])
+
+fig, ax = plt.subplots(2, 1, figsize=(14, 8))
+ax[0].plot(log_energie(sig, f, win=8192, step=4096))
+# plt.show()
+ax[1].plot(time, f0_filt)
+plt.show()
 # dict_ = {}
 
 # for filename in files:
---
+++ src/features_extraction/audio/parse_LIUM.py
@@ -0,0 +1,8 @@
+import os
+from scipy.io.wavfile import read
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.io.wavfile import 
+
