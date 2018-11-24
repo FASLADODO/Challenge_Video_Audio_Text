5a00abf0d76cbbd612628dd3215c473b1460415b
Mickael-Anj <mickael.anjoran@orange.fr>
2018-11-24 17:26:49 +0100

    Add files via upload

--- src/models/text/kmeans_nmf.py
+++ src/models/text/kmeans_nmf.py
@@ -20,11 +20,12 @@ import numpy as np
 warnings.filterwarnings('ignore')
 from plt import plot_cluster
 from evaluation_cluster import best_k
+from sklearn.manifold import TSNE
 
 N_COMPONENTS = 2
 N_CLUSTERS = 5
 
-data = pd.read_csv(f'/home/mickael/Documents/Challenge_Video_Audio_Text/features/text/nmf_tfidf_{N_COMPONENTS}.csv',sep='§')
+data = pd.read_csv(f'/home/mickael/Documents/Challenge_Video_Audio_Text/features/text/nmf_tfidf_{N_COMPONENTS}_TSNE.csv',sep='§')
 
 model = KMeans(n_clusters=N_CLUSTERS,random_state=42,n_init=30)
 
@@ -35,7 +36,7 @@ data['cluster'] = cluster
 #plt.scatter(data['nmf_0'], data['nmf_1'], c=cluster, s=50, cmap='viridis')
 #plt.show()
 
-plot_cluster(data[['nmf_0','nmf_1']].values, data['Sequence'],cluster, '/home/mickael/Documents/Challenge_Video_Audio_Text/result/plot_cluster_nmf_5.html')
+plot_cluster(data[['nmf_0','nmf_1']].values, data['Sequence'],cluster, '/home/mickael/Documents/Challenge_Video_Audio_Text/result/plot_cluster_nmf_5_TSNE.html')
 
 
 #best_k(data.drop(['Sequence'], axis='columns'), range_min = 20, verbose = True)
